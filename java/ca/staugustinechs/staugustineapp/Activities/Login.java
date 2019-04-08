445af8eb5b616cf2575289a611e068fd5634c342
pabloj2001 <46763241+pabloj2001@users.noreply.github.com>
2019-04-08 14:13:42 -0400

    1.05 Commenting

--- java/ca/staugustinechs/staugustineapp/Activities/Login.java
+++ java/ca/staugustinechs/staugustineapp/Activities/Login.java
@@ -56,9 +56,11 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         getWindow().setNavigationBarColor(AppUtils.PRIMARY_DARK_COLOR);
         getWindow().setStatusBarColor(AppUtils.PRIMARY_DARK_COLOR);
 
+        //SET TITLE AND ACTION BAR COLORS
         this.getSupportActionBar().setTitle(R.string.app_name);
         this.getSupportActionBar().setBackgroundDrawable(new ColorDrawable(AppUtils.PRIMARY_COLOR));
 
+        //SET UP SIGN IN BUTTON
         signInButton = (SignInButton) findViewById(R.id.signInButton);
         signInButton.setOnClickListener(this);
     }
@@ -70,24 +72,32 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 .requestEmail()
                 .build();
 
+        //SIGN ALL USERS OUT OF THE GOOGLE CLIENT SO IT DOESN'T AUTOMATICALLY
+        //TRY TO SIGN IN WITH THE LAST GOOGLE ACCOUNT USED AND USER CAN CHOOSE WHICH
+        //GOOGLE ACCOUNT TO USE.
         GoogleSignInClient googleClient = GoogleSignIn.getClient(this, gso);
         googleClient.signOut();
 
-        //CREATE SIGN IN WINDOW
+        //CREATE SIGN IN WINDOW AND CALL OUR onActivityResult METHOD WHEN DONE
         startActivityForResult(googleClient.getSignInIntent(), RC_SIGN_IN);
     }
 
     // [START auth_fui_result]
+    //THIS METHOD GETS CALLED WHEN SIGN IN INTENT IS FINISHED
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        
         if (requestCode == RC_SIGN_IN) {
+            //GET THE GOOGLE SIGN IN TASK RESULT
             Task<GoogleSignInAccount> googleTask = GoogleSignIn.getSignedInAccountFromIntent(data);
             if (googleTask.isSuccessful()) {
                 // Successfully signed in
+                //GET THE GOOGLE ACCOUNT
                 GoogleSignInAccount account = googleTask.getResult();
+                //ENSURE ACCOUNT EMAIL CONTAINS @ycdsbk12.ca
                 if(account.getEmail().contains("@ycdsbk12.ca") || AppUtils.ALLOW_ACCOUNTS){
+                    //SIGN INTO FIREBASE USING OUR GOOGLE ACCOUNT CREDENTIALS
                     FirebaseAuth.getInstance()
                             .signInWithCredential(GoogleAuthProvider.getCredential(account.getIdToken(), null))
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -118,6 +128,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     }
 
     private void finishSignIn(){
+        //GET THE USER DOCUMENT
         FirebaseFirestore.getInstance().collection("users")
                 .document(FirebaseAuth.getInstance().getUid())
                 .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -126,6 +137,10 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 if(task.isSuccessful()){
                     Intent intent = null;
                     if(task.getResult().exists()){
+                        //IF THE USER'S DOCUMENT EXISTS (IF THE USER HAS SIGNED IN BEFORE),
+                        //TAKE THEM TO THE MAIN ACTIVITY.
+                        //BEFORE DOING THAT, CHECK IF THE FILE SAYING THAT WE HAVE SIGNED UP EXISTS,
+                        //AND IF IT DOESN'T, MAKE THE FILE.
                         if(AppUtils.shouldGetFile(FirebaseAuth.getInstance().getUid() + SignUp.SIGNUP_FILE,
                                 Login.this)){
                             //SAVE THAT USER HAS SIGNED UP
@@ -136,6 +151,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                         }
                         intent = new Intent(Login.this, Main.class);
                     }else{
+                        //TAKE THE USER TO THE SIGN UP ACTIVITY
                         intent = new Intent(Login.this, SignUp.class);
                     }
                     Login.this.startActivity(intent);
