de2d70253235afce7c30157325527295c6b7154f
Mehul Pillai <mehulpillai30@gmail.com>
2019-10-22 21:12:26 -0400

    added dark theme application on launch

--- app/src/main/java/ca/staugustinechs/staugustineapp/Activities/Launcher.java
+++ app/src/main/java/ca/staugustinechs/staugustineapp/Activities/Launcher.java
@@ -1,22 +1,34 @@
 package ca.staugustinechs.staugustineapp.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 import ca.staugustinechs.staugustineapp.AppUtils;
+import ca.staugustinechs.staugustineapp.Fragments.SettingsFragment;
 import ca.staugustinechs.staugustineapp.R;
 
 public class Launcher extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        SharedPreferences sharedPrefs = getSharedPreferences(SettingsFragment.sharedPrefFile, MODE_PRIVATE);
+        boolean isDark = sharedPrefs.getBoolean(SettingsFragment.DARK_KEY, false);
+        if (isDark) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            getDelegate().applyDayNight();
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            getDelegate().applyDayNight();
+        }
         //FIREBASE REMOTE CONFIG
         final FirebaseRemoteConfig frc = FirebaseRemoteConfig.getInstance();
         //IF IT HAS BEEN LESS THAN 360 SECONDS, WE DON'T GET THE LATEST RC VALUES
