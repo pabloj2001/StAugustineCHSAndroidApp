5cc08cd4cdfc76c5c1864706d48024a451b4cd48
Mehul Pillai <mehulpillai30@gmail.com>
2020-01-07 19:47:17 -0500

    Moves faq and spirit info to firebase

--- app/src/main/java/ca/staugustinechs/staugustineapp/Fragments/SpiritMetersFragment.java
+++ app/src/main/java/ca/staugustinechs/staugustineapp/Fragments/SpiritMetersFragment.java
@@ -1,5 +1,6 @@
 package ca.staugustinechs.staugustineapp.Fragments;
 
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,16 +16,31 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+
 import ca.staugustinechs.staugustineapp.AppUtils;
 import ca.staugustinechs.staugustineapp.AsyncTasks.GetSpiritPointsTask;
+import ca.staugustinechs.staugustineapp.Objects.QuestionAnswer;
 import ca.staugustinechs.staugustineapp.R;
 
 public class SpiritMetersFragment extends Fragment implements View.OnClickListener {
     private CardView[] levelCards = new CardView[4];
+    private ArrayList<QuestionAnswer> spiritInfoList = new ArrayList<>();
+    private ArrayList<String> spiritLevelTitles = new ArrayList<>();
+    private ArrayList<String> spiritLevelDesc = new ArrayList<>();
     private boolean[] expanded = new boolean[4];
     private TextView grade9text, grade10text, grade11text, grade12text;
     private ProgressBar grade9bar, grade10bar, grade11bar, grade12bar;
     private GetSpiritPointsTask task;
+    private GetSpiritInfoTask infoTask;
 
     @Nullable
     @Override
@@ -34,6 +50,10 @@ public class SpiritMetersFragment extends Fragment implements View.OnClickListen
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        initializeCards(view);
+        setCardVisibility(View.GONE);
+
         if(AppUtils.isNetworkAvailable(this.getActivity())){
             grade9text = view.findViewById(R.id.grade9text);
             grade10text = view.findViewById(R.id.grade10text);
@@ -51,6 +71,9 @@ public class SpiritMetersFragment extends Fragment implements View.OnClickListen
 
             task = new GetSpiritPointsTask(this);
             task.execute();
+
+            infoTask = new GetSpiritInfoTask();
+            infoTask.execute();
         } else {
             setOffline();
         }
@@ -99,25 +122,35 @@ public class SpiritMetersFragment extends Fragment implements View.OnClickListen
 
     }
 
-    private void initializeCards(View view) {
-        String[] spiritLevelTitles = getResources().getStringArray(R.array.spirit_level_titles);
-        String[] spiritLevelDesc = getResources().getStringArray(R.array.spirit_level_desc);
-        levelCards[0] = view.findViewById(R.id.level_1);
-        levelCards[1] = view.findViewById(R.id.level_2);
-        levelCards[2] = view.findViewById(R.id.level_3);
-        levelCards[3] = view.findViewById(R.id.level_4);
+    private void setCardVisibility(int visibility) {
+        for (CardView card : levelCards) {
+            card.setVisibility(visibility);
+        }
+    }
 
-        for (int i = 0; i < levelCards.length; i++) {
+    private void updateCards() {
+        for (int i = 0; i < spiritInfoList.size(); i++) {
+            String title = spiritInfoList.get(i).getQuestion().
+                    replaceAll("-n", Objects.requireNonNull(System.getProperty("line.separator")));
+            String desc = spiritInfoList.get(i).getAnswer().
+                    replaceAll("-n", Objects.requireNonNull(System.getProperty("line.separator")));
             expanded[i] = false;
             TextView titleTv = levelCards[i].findViewById(R.id.question);
             TextView bodyTv = levelCards[i].findViewById(R.id.answer);
             ImageView expandCollapse = levelCards[i].findViewById(R.id.expand_collapse);
-            titleTv.setText(spiritLevelTitles[i]);
-            bodyTv.setText(spiritLevelDesc[i]);
+            titleTv.setText(title);
+            bodyTv.setText(desc);
             expandCollapse.setBackgroundResource(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO
                     ? R.drawable.ic_expand_24dp : R.drawable.ic_expand_dark_24dp);
             levelCards[i].setOnClickListener(this);
         }
+    }
+
+    private void initializeCards(View view) {
+        levelCards[0] = view.findViewById(R.id.level_1);
+        levelCards[1] = view.findViewById(R.id.level_2);
+        levelCards[2] = view.findViewById(R.id.level_3);
+        levelCards[3] = view.findViewById(R.id.level_4);
     }
 
     @Override
@@ -149,6 +182,11 @@ public class SpiritMetersFragment extends Fragment implements View.OnClickListen
             if(task != null){
                 task.cancel(true);
             }
+
+
+            if (infoTask != null) {
+                infoTask.cancel(true);
+            }
         }
     }
 
@@ -156,5 +194,38 @@ public class SpiritMetersFragment extends Fragment implements View.OnClickListen
     public void onDetach(){
         onHiddenChanged(true);
         super.onDetach();
+    }
+
+    private class GetSpiritInfoTask extends AsyncTask<Void, Void, ArrayList<QuestionAnswer>> {
+
+        @Override
+        protected ArrayList<QuestionAnswer> doInBackground(Void... voids) {
+            Task<DocumentSnapshot> spiritInfoTask = FirebaseFirestore.getInstance().collection("info")
+                    .document("spiritInfo").get();
+            while (!spiritInfoTask.isComplete()) {
+            }
+
+            if (spiritInfoTask.isSuccessful()) {
+                for (Map.Entry<String, Object> entry :
+                        (Objects.requireNonNull(Objects.requireNonNull(spiritInfoTask.getResult()).getData())).entrySet()) {
+                    spiritInfoList.add(new QuestionAnswer(entry.getKey(), (String) entry.getValue()));
+                }
+            }
+
+            Collections.sort(spiritInfoList, new Comparator<QuestionAnswer>() {
+                @Override
+                public int compare(QuestionAnswer o1, QuestionAnswer o2) {
+                    return o1.getQuestion().compareTo(o2.getQuestion());
+                }
+            });
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<QuestionAnswer> spiritInfo) {
+            setCardVisibility(View.VISIBLE);
+            updateCards();
+        }
     }
 }
